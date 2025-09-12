55cea3ddb04e9cc6149414ee6d1f4aab0460cdcf
HDTHREE <hd3nni4@gmail.com>
2025-09-12 17:16:44 -0400

    built project, script for venv, .gitignore every single julia and r file (shit language)

--- dash_jsonschema/__init__.py
+++ dash_jsonschema/__init__.py
@@ -10,45 +10,45 @@ import dash as _dash
 from ._imports_ import *
 from ._imports_ import __all__
 
-if not hasattr(_dash, '__plotly_dash') and not hasattr(_dash, 'development'):
-    print('Dash was not successfully imported. '
-          'Make sure you don\'t have a file '
-          'named \n"dash.py" in your current directory.', file=_sys.stderr)
+if not hasattr(_dash, "__plotly_dash") and not hasattr(_dash, "development"):
+    print("Dash was not successfully imported. "
+          "Make sure you don\"t have a file "
+          "named \n"dash.py" in your current directory.", file=_sys.stderr)
     _sys.exit(1)
 
-_basepath = _os.path.dirname(__file__)
-_filepath = _os.path.abspath(_os.path.join(_basepath, 'package-info.json'))
+_basepath: str = _os.path.dirname(__file__)
+_filepath: str = _os.path.abspath(_os.path.join(_basepath, "package-info.json"))
 with open(_filepath) as f:
-    package = json.load(f)
+    package: dict[str] = json.load(f)
 
-package_name = package['name'].replace(' ', '_').replace('-', '_')
-__version__ = package['version']
+package_name: str = package["name"].replace(" ", "_").replace("-", "_")
+__version__ = package["version"]
 
-_current_path = _os.path.dirname(_os.path.abspath(__file__))
+_current_path: str = _os.path.dirname(_os.path.abspath(__file__))
 
-_this_module = _sys.modules[__name__]
+_this_module: str = _sys.modules[__name__]
 
-_js_dist = []
-
-_js_dist.extend(
-    [
-        {
-            'relative_package_path': 'dash_jsonschema.js',
-    
-            'namespace': package_name
-        },
-        {
-            'relative_package_path': 'dash_jsonschema.js.map',
-    
-            'namespace': package_name,
-            'dynamic': True
-        }
-    ]
-)
+_js_dist: list[dict[str] = [
+    {
+        "relative_package_path": "dash_jsonschema.js",
+        "namespace": package_name
+    },
+    {
+        "relative_package_path": "dash_jsonschema.js.map",
+        "namespace": package_name,
+        "dynamic": True
+    },
+    {
+        dev_package_path: "proptypes.js",
+        dev_only: True,
+        namespace: "dash_jsonschema"
+    }
+]
 
 _css_dist = []
 
 
 for _component in __all__:
-    setattr(locals()[_component], '_js_dist', _js_dist)
-    setattr(locals()[_component], '_css_dist', _css_dist)
+    setattr(locals()[_component], "_js_dist", _js_dist)
+    setattr(locals()[_component], "_css_dist", _css_dist)
+
