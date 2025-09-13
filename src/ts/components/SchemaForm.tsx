2256e0f7a58b6e11efe81a91ae2e091958136bdd
HDTHREE <hd3nni4@gmail.com>
2025-09-12 22:07:07 -0400

    fixed the validation bug

--- src/ts/components/SchemaForm.tsx
+++ src/ts/components/SchemaForm.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import {DashComponentProps} from "../props";
+import { DashComponentProps } from "../props";
 import Form from "@rjsf/core";
 import { RJSFSchema } from "@rjsf/utils";
 import validator from "@rjsf/validator-ajv8";
+import draft6MetaSchema from "ajv/dist/refs/json-schema-draft-06.json";
+
+
+validator.ajv.addMetaSchema(draft6MetaSchema);
+
 
 export type Props = {
     schema: RJSFSchema;
 } & DashComponentProps;
 
-const SchemaForm: React.FC<Props> = ({id = undefined, schema = {}}: Props) => <Form id={id} schema={schema} validator={validator}/>;
+
+const SchemaForm: React.FC<Props> = ({ id = undefined, schema = {} }: Props) => <Form id={id} schema={schema} validator={validator} />;
+
 
 export default SchemaForm;
