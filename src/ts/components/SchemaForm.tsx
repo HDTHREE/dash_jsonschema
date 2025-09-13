c3aeecca1cd76322e4d8f8010edfc47f3f9f5b64
HDTHREE <hd3nni4@gmail.com>
2025-09-13 12:10:47 -0400

    everything done, todo building

--- src/ts/components/SchemaForm.tsx
+++ src/ts/components/SchemaForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { DashComponentProps } from "../props";
-import Form from "@rjsf/core";
+import Form, { FormProps, ThemeProps, withTheme } from "@rjsf/core";
 import { RJSFSchema } from "@rjsf/utils";
 import validator from "@rjsf/validator-ajv8";
 import draft6MetaSchema from "ajv/dist/refs/json-schema-draft-06.json";
@@ -9,12 +9,38 @@ import draft6MetaSchema from "ajv/dist/refs/json-schema-draft-06.json";
 validator.ajv.addMetaSchema(draft6MetaSchema);
 
 
-export type Props = {
+const themes: ReadonlyArray<string> = ["antd", "chakra-ui", "mantine", "mui", "primereact", "react-bootstrap", "semantic-ui", "shadcn"] as const;
+type Theme = typeof themes[number];
+
+
+type Props = {
+    /*
+     * The JSON Schema object to generate the form.
+     */
     schema: RJSFSchema;
+    /*
+     * The theme to use for rendering the form.
+     */
+    theme?: Theme;
 } & DashComponentProps;
 
 
-const SchemaForm: React.FC<Props> = ({ id = undefined, schema = {} }: Props) => <Form id={id} schema={schema} validator={validator} />;
+const getThemedForm: (theme: Theme) => React.ComponentType<FormProps<any, RJSFSchema> & ThemeProps> = theme => {
+    const requirePath = `@rjsf/${theme}` as const;
+    const themeObject: ThemeProps = require(requirePath).Theme as ThemeProps;
+
+    return withTheme(themeObject);
+};
+
+
+/**
+ * A form component that generates a form based on a JSON Schema.
+ */
+const SchemaForm: React.FC<Props> = ({ id = null, setProps, schema = {}, theme = null }: Props) => {
+    const CurrentForm: React.ComponentType<FormProps<any, RJSFSchema> & ThemeProps> = theme && themes.includes(theme) ? getThemedForm(theme) : Form;
+
+    return <CurrentForm id={id} schema={schema} validator={validator} />;
+};
 
 
 export default SchemaForm;
--- usage.py
+++ usage.py
@@ -17,4 +17,4 @@ app.layout = dj.SchemaForm(id="component", schema=Person.json_schema())
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
