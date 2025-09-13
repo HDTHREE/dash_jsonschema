30bf9bfc10fb88ecc7b07f64cca72ad1f20c2e99
HDTHREE <hd3nni4@gmail.com>
2025-09-12 21:43:22 -0400

    i give up on theme

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,4 @@
 import SchemaForm from "./components/SchemaForm";
-import MuiForm from "./components/MuiForm";
-import ShadForm from "./components/ShadForm";
-import AntDForm from "./components/AntDForm";
 
 export {
     SchemaForm
